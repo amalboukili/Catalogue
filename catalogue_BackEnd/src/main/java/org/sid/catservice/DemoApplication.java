498ded9e4fe9515871da757f7f983538383e8aff
amalboukili <boukili.amal@gmail.com>
2019-07-22 14:47:22 +0000

    Chercher un produit par page & exposer ID

--- catalogue_BackEnd/src/main/java/org/sid/catservice/DemoApplication.java
+++ catalogue_BackEnd/src/main/java/org/sid/catservice/DemoApplication.java
@@ -6,17 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 	@Autowired
 	private ProduitRepository produitRepository;
+	@Autowired
+	private RepositoryRestConfiguration restConfiguration;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		restConfiguration.exposeIdsFor(Produit.class);
 		produitRepository.save(new Produit(null, "Ordinateur hp 45", 6700.00, 3)); 
 		produitRepository.save(new Produit(null, "Imprimante Canon", 1700, 3)); 
 		produitRepository.save(new Produit(null, "Smart Phone HUAWEI Y7", 1800, 13)); 
--- catalogue_BackEnd/src/main/java/org/sid/catservice/dao/ProduitRepository.java
+++ catalogue_BackEnd/src/main/java/org/sid/catservice/dao/ProduitRepository.java
@@ -3,6 +3,8 @@ package org.sid.catservice.dao;
 import java.util.List;
 
 import org.sid.catservice.entities.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -12,4 +14,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 public interface ProduitRepository extends JpaRepository<Produit,Long>{
     @RestResource(path = "/byDesignation")
     public List<Produit> findByDesignationContains(@Param("mc") String des);
+
+    @RestResource(path = "/byDesignationPage")
+    public Page<Produit> findByDesignationContains(@Param("mc") String des, Pageable pageable);
 }
