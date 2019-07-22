0b29427d93f47de29f76460caa68cbbb5bfa7647
amalboukili <boukili.amal@gmail.com>
2019-07-22 13:49:59 +0000

    catalogue des produits

--- catalogue_BackEnd/src/main/java/org/sid/catservice/DemoApplication.java
+++ catalogue_BackEnd/src/main/java/org/sid/catservice/DemoApplication.java
@@ -1,13 +1,30 @@
 package org.sid.catservice;
 
+import org.sid.catservice.dao.ProduitRepository;
+import org.sid.catservice.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class DemoApplication implements CommandLineRunner {
+	@Autowired
+	private ProduitRepository produitRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		produitRepository.save(new Produit(null, "Ordinateur hp 45", 6700.00, 3)); 
+		produitRepository.save(new Produit(null, "Imprimante Canon", 1700, 3)); 
+		produitRepository.save(new Produit(null, "Smart Phone HUAWEI Y7", 1800, 13)); 
+
+		produitRepository.findAll().forEach(p->{
+			System.out.println(p.toString());
+		});
+	}
+
 
 }
