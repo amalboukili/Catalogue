7b4166b35920cb64f4ec8993f2c864d72ec79d18
amalboukili <boukili.amal@gmail.com>
2019-07-22 14:18:13 +0000

    Chercher un produit

--- catalogue_BackEnd/src/main/java/org/sid/catservice/dao/ProduitRepository.java
+++ catalogue_BackEnd/src/main/java/org/sid/catservice/dao/ProduitRepository.java
@@ -1,10 +1,15 @@
 package org.sid.catservice.dao;
 
+import java.util.List;
+
 import org.sid.catservice.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface ProduitRepository extends JpaRepository<Produit,Long>{
-
+    @RestResource(path = "/byDesignation")
+    public List<Produit> findByDesignationContains(@Param("mc") String des);
 }
