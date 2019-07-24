e7ecd3246309d6419bcef5af83c3013a5012a2c5
amalboukili <boukili.amal@gmail.com>
2019-07-24 15:04:36 +0000

    chercher un produit & pagination

--- catalogue_FrontEnd/src/app/produits/produits.component.ts
+++ catalogue_FrontEnd/src/app/produits/produits.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produits.component.css']
 })
 export class ProduitsComponent implements OnInit {
+  
   public produits:any;
+  public size:number=5;
+  public currentPage:number=0;
+  public totalPages:number;
+  public pages:Array<number>;
+  currentKeyword: string="";
+
+
   constructor(private CatService:CatalogueService) { }
 
   ngOnInit() {
@@ -15,12 +23,48 @@ export class ProduitsComponent implements OnInit {
   }
 
   onGetProducts(){
-    this.CatService.getProducts()
+    this.CatService.getProducts(this.currentPage,this.size)
     .subscribe(data=>{
+      this.totalPages=data["page"].totalPages;
+      this.pages=new Array<number>(this.totalPages);
       this.produits=data;
     },err=>{
       console.log(err);
     });
   }
+
+  onPageProduct(i){
+    this.currentPage=i;
+    this.chercherProduits();
+  }
+
+  onChercher(form :any){
+    this.currentPage=0;
+    this.currentKeyword=form.keyword;
+    this.chercherProduits();
+  }
+
+  chercherProduits(){
+    this.CatService.getPruductsByKeyWord(this.currentKeyword ,this.currentPage,this.size)
+    .subscribe(data=>{
+      this.totalPages=data["page"].totalPages;
+      this.pages=new Array<number>(this.totalPages);
+      this.produits=data;
+    },err=>{
+      console.log(err);
+    });
+    }
+
+    onDeleteProduct(p){
+      let conf=confirm("Vous êtes entrain de supprimer ce produit, êtes vous sur???");
+      if(conf){
+        this.CatService.deleteResource(p._links.self.href)
+        .subscribe(data=>{
+          this.chercherProduits();
+        }, err=>{
+          console.log(err);
+        })
+      }
+    }
 
 }
--- catalogue_FrontEnd/src/app/services/catalogue.service.ts
+++ catalogue_FrontEnd/src/app/services/catalogue.service.ts
@@ -6,9 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class CatalogueService {
 
+  public host:string="http://localhost:8081";
+
   constructor(private httpClient:HttpClient) { }
 
-  public getProducts(){
-    return this.httpClient.get("http://localhost:8081/produits");
+  public getProducts(page:number, size:number){
+    return this.httpClient.get(this.host+"/produits?page="+page+"&size="+size);
   }
+
+  public getPruductsByKeyWord(mc:string, page:number, size:number){
+    return this.httpClient.get(this.host+"//produits/search/byDesignationPage?mc="+mc+"&page="+page+"&size="+size);
+  }
+  
+  public deleteResource(url){
+    return this.httpClient.delete(url);
+  }
+
 }
